13809b53896331a624241a8487457efbba19527e
Shyam Patidar <anu.shyam.patidar@gmail.com>
2025-08-26 11:34:59 +0530

    new upload

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,10 +1,10 @@
-import { connectMongoDB } from "@/lib/mongodb";
+import {connectMongoose} from "@/lib/mongodb";
 import AdminInfo from "@/models/Admin";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
     try {
-        await connectMongoDB();
+        await connectMongoose();
         const { email } = await req.json();
         const admin = await AdminInfo.findOne({ email }).select("_id");
         console.log("krishna-academy-admin: ", admin);
